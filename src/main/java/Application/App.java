ada989913fe91221daeea3b7e58fb2bb8f4e27f7
zhaotong <shamingai@foxmail.com>
2018-09-29 00:26:20 +0800

    scan static

--- src/main/java/Application/App.java
+++ src/main/java/Application/App.java
@@ -1,8 +1,11 @@
+package Application;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 启动方式2/2 可启用同层次的包，默认扫描启用同层次的包
+ * http://127.0.0.1:8080/hello
  */
 @SpringBootApplication
 public class App {
@@ -10,3 +13,9 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 }
+
+/**
+ * SpringBoot默认扫描静态文件：
+ * 配置：默认扫描 applicatin.properties
+ * 静态文件：默认扫描 resources/static 下的文件 js\css\image
+ */
