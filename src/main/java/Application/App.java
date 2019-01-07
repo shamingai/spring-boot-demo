fa2a932d1ada734717e5f489bad2cc868d59b847
zhaotong <shamingai@foxmail.com>
2019-01-07 13:07:13 +0800

    banner

--- src/main/java/Application/App.java
+++ src/main/java/Application/App.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 启动方式2/2 可启用同层次的包，默认扫描启用同层次的包
  * http://127.0.0.1:8080/hello
  */
-@SpringBootApplication
-public class App {
+@SpringBootApplication // Spring Boot项目的核心注解，主要目的是开启自动配置，它其实是一个组合注解。
+public class App { // Spring Boot的项目一般都会有*Application的入口类，入口类中会有main方法，这是一个标准的Java应用程序的入口方法。
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
